4c7d7325d2652b180678b3539c4b9d8ff608d55e
siddheswar <siddheswar@siddheswar-HP-15-Notebook-PC>
2019-01-27 21:13:35 +0530

    second commit

--- TDC/src/Runner.java
+++ TDC/src/Runner.java
@@ -14,21 +14,53 @@ public class Runner {
 		File file = new File(file_name);
 		int result;
 		try {
+			drop_backup();
+			move_to_backup();
 			Scanner is = new Scanner(file);
 			while(is.hasNext()) {
-				String data = is.next();
+				String data = is.nextLine();
 				String[] values = data.split(",");
 				result = addData(values);
+				System.out.println(values[0]+" "+values[1]);
 			}
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private static void move_to_backup() {
+		Connection con = DatabaseUtil.getConnection();
+		PreparedStatement ps = null;
+		String query1 = "create table hhs_data_bk(hlpi_name varchar(50),year varchar(4),hlpi varchar(50),oth varchar(10),tot_hhs varchar(50),own varchar(50))";
+		String query2 = "insert into hhs_data_bk select * from hhs_data";
+		String query3 = "truncate table hhs_data";
+		try {
+			ps = con.prepareStatement(query1);
+			ps.executeUpdate();
+			ps = con.prepareStatement(query2);
+			ps.executeUpdate();
+			ps = con.prepareStatement(query3);
+			ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void drop_backup() {
+		Connection con = DatabaseUtil.getConnection();
+		PreparedStatement ps = null;
+		String query = "drop table hhs_data_bk";
+		try {
+			ps = con.prepareStatement(query);
+			ps.executeUpdate();
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
 	private static int addData(String[] values) {
 		Connection con = DatabaseUtil.getConnection();
-		PreparedStatement ps=null;
+		PreparedStatement ps = null;
 		String query = "insert into hhs_data(hlpi_name,year,hlpi,tot_hhs,own) values(?,?,?,?,?)";
 		try {
 			ps = con.prepareStatement(query);
@@ -39,7 +71,6 @@ public class Runner {
 			ps.setString(5, values[4]);
 			ps.executeUpdate();
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return 0;
